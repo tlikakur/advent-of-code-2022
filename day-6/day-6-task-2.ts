429e3e34a1cb6f74937f05f0d3781a7d9466b02a
Tomislav Likakur <tomislav.likakur@gmail.com>
2022-12-10 21:59:07 +0100

    add: day 6

---
+++ day-6/day-6-task-2.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import { readInput } from "../util";
+
+const data = readInput(path.resolve(__dirname, "day-6-data.txt"));
+
+const MARKER_LENGTH = 14;
+let startOfPacketIndex = MARKER_LENGTH;
+
+for (let i = MARKER_LENGTH; i < data.length; i++) {
+  const substr = data.slice(i - MARKER_LENGTH, i);
+  if (new Set(substr).size === MARKER_LENGTH) break;
+  startOfPacketIndex++;
+}
+
+console.log(`Start of packet index: ${startOfPacketIndex}`);
